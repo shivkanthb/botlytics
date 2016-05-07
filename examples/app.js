5b6acfd6fd3194416962a278e92c83658e2e40f9
shivkanthb <shivkanthb@gmail.com>
2016-05-06 21:18:47 -0700

    tests complete

--- examples/app.js
+++ examples/app.js
@@ -5,18 +5,14 @@ var bot_token="";  //include your bot token here
 
 botlytics.setBotToken(bot_token);
 
-botlytics.incoming("callback testing123","user_123", function(err,res, body){
-    // console.log("Callback should work hopefully");
+botlytics.incoming("test js testing ","user_123", function(err,res, body){
     if(err) throw Error(err);
     console.log(body);
 });
 
 
-// botlytics.outgoing("Thanks for trying Botlytics","user_123", function(err,res, body){
-//     // console.log("Callback should work hopefully");
-//     if(err) throw Error(err);
-//     console.log(body);
-// });
+botlytics.outgoing("Thanks for trying Botlytics test","user_123", function(err,res, body){
+    if(err) throw Error(err);
+    console.log(body);
+});
 
-
-// console.log(botlytics.test());
