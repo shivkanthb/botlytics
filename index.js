1cd91e66e0c07fe6b5793b461ffc4ec707cf1639
shivkanthb <shivkanthb@gmail.com>
2017-05-03 03:52:50 -0700

    botlytics api parameters update

--- index.js
+++ index.js
@@ -4,46 +4,46 @@ var request = require("request");
 
 module.exports = {
     
-incoming : function(input, conv_id, callback) {
-    
+incoming : function(input, conv_id, sender_id, platform, payload, callback) {
     var options = { 
-    method: 'POST',
-    url: 'http://www.botlytics.co/api/v1/messages?token='+bot_token,
-    json: true,
-    body: {
-        message: {
-            text: input,
-            kind: "incoming",
-            conversation_identifier: conv_id
+        method: 'POST',
+        url: 'http://www.botlytics.co/api/v1/messages?token='+bot_token,
+        json: true,
+        body: {
+            message: {
+                text: input,
+                kind: "incoming",
+                conversation_identifier: conv_id,
+                sender_identifier: sender_id,
+                platform: platform,
+                payload: payload
+            }
         }
-    }
-};
-    
+    };
     request(options, function (err, res, body) {
         callback(err, res, body);
     });
-    
 },
 
-outgoing: function(output, conv_id, callback) {
-    
+outgoing: function(output, conv_id,sender_id, platform, payload,  callback) {
     var options = { 
-    method: 'POST',
-    url: 'http://www.botlytics.co/api/v1/messages?token='+bot_token,
-    json: true,
-    body: {
-        message: {
-            text: output,
-            kind: "outgoing",
-            conversation_identifier: conv_id
+        method: 'POST',
+        url: 'http://www.botlytics.co/api/v1/messages?token='+bot_token,
+        json: true,
+        body: {
+            message: {
+                text: output,
+                kind: "outgoing",
+                conversation_identifier: conv_id,
+                sender_identifier: sender_id,
+                platform: platform,
+                payload: payload
+            }
         }
-    }
-};
-    
+    };
     request(options, function (err, res, body) {
         callback(err, res, body);
     });
-    
 },
 
 setBotToken: function(token) {
--- test/test.js
+++ test/test.js
@@ -4,20 +4,20 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var botlytics = require('../index');
-var bot_token = ""; //bot token comes here
+var bot_token = "f8695b4d4c9e4107"; //bot token comes here
 
 describe('botlytics', function() {
   
     it('should return an object for incoming', function() {
-       botlytics.setBotToken('f8695b4d4c9e4107');
-       var res = botlytics.incoming("hello","user_123", function(err, resp,body) {
+       botlytics.setBotToken(bot_token);
+       var res = botlytics.incoming("hello","user_123","sender1","messenger",null, function(err, resp,body) {
            expect(res.body).to.have.property('text'); 
        });    
     });
     
       it('should return an object for outgoing', function() {
        botlytics.setBotToken(bot_token);
-       var res = botlytics.outgoing("hello hooman","user_123", function(err, resp,body) {
+       var res = botlytics.outgoing("hello hooman","user_123","sender1","messenger",null, function(err, resp,body) {
            expect(res.body).to.have.property('text'); 
        });    
     });
